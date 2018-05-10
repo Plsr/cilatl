9254db879b5b18383c4bcc2fd65816eafb6058bf
Plsr <christianpoplawski@gmail.com>
2018-05-10 16:21:12 +0200

    Add index for bookmarks
    Also, some changes regarding validation on the way

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,5 @@
 class BookmarksController < ApplicationController
+  before_action :scrub_fields_array, only: [:create, :update], if: -> { :has_fields_param? }
   def new
     @bookmark = Bookmark.new
   end
@@ -7,15 +8,44 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.find(params[:id])
   end
 
-  def edit; end
+  def edit
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def index
+    @bookmarks = Bookmark.all.limit(20)
+  end
 
   def create
-    Bookmark.create(bookmark_params)
+    @bookmark = Bookmark.new(bookmark_params)
+
+    if @bookmark.save
+      redirect_to @bookmark
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @bookmark = Bookmark.find(params[:id])
+    if @bookmark.update_attributes(bookmark_params)
+      redirect_to @bookmark
+    else
+      render "new"
+    end
   end
 
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:title, :url, :description, :media_type, :fields)
+    params.require(:bookmark).permit(:title, :url, :description, :media_type, fields: [])
+  end
+
+  def scrub_fields_array
+    params[:bookmark][:fields].delete_if(&:blank?)
+  end
+
+  def has_fields_param?
+    params[:bookmark] && params[:bookmark][:field]
   end
 end
