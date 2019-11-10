b4c4a3dbdee9226656f2dd918cb46b4fd8254230
Christian Poplawski <christianpoplawski@gmail.com>
2019-11-10 13:15:13 +0100

    Add body_text field to bookmarks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_01_091325) do
+ActiveRecord::Schema.define(version: 2019_11_10_121232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_11_01_091325) do
     t.boolean "archived", default: false, null: false
     t.datetime "archived_on"
     t.bigint "media_type_id"
+    t.text "body_text"
     t.index ["media_type_id"], name: "index_bookmarks_on_media_type_id"
   end
 
