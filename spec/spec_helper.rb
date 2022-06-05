148fa094b463f89c16e91efed031c2286456aadf
Plsr <christianpoplawski@gmail.com>
2022-06-05 08:02:31 +0200

    Fix database cleaner config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -69,7 +69,7 @@ RSpec.configure do |config|
   end
 
   config.before(:each) do
-    DatabaseCleaner.strategy = 'transaction'
+    DatabaseCleaner.strategy = :transaction
   end
 
   config.before(:each, type: :feature) do
